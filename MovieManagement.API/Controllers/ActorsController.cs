8c2b5ef160daa3168ccc6f23642250650387e6b4
GLARIS HILDA T <glaristhomas@gmail.com>
2023-11-26 15:06:43 +0530

    Add project files.

---
+++ MovieManagement.API/Controllers/ActorsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieManagement.Domain.Repository;
+
+namespace MovieManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ActorsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var actorFromRepo = _unitOfWork.Actor.GetAll();
+            return Ok(actorFromRepo);
+        }
+
+        [HttpGet("Movies")]
+        public ActionResult GetWithMovies()
+        {
+            var actorFromRepo = _unitOfWork.Actor.GetActorsWithMovies();
+            return Ok(actorFromRepo);
+        }
+    }
+}
